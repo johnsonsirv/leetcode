0858c348684a084119ba94359e78d6f3fb90b952
Victor Johnson <johnsonsirv@gmail.com>
2019-07-30 10:56:04 +0200

    add two numbers with Linked list

---
+++ add_two_numbers.rb
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+class ListNode
+    attr_accessor :val, :next
+    def initialize(val)
+        @val = val
+        @next = nil
+    end
+
+    def reverse
+      
+    end
+end
+
+# @param {ListNode} l1
+# @param {ListNode} l2
+# @return {ListNode}
+def add_two_numbers(l1, l2)
+    #travers the list and grab nodes in reverse order, return reversed
+    # store in a list and perform addition
+    # for each digit in sum <list> reverse
+    # return linkedlsit
+    node = ListNode.new()
+    l1_reversed = 
+end
