6565ab51c44035617752daa0557657c66eecbc73
Victor Johnson <johnsonsirv@gmail.com>
2019-07-31 12:09:36 +0200

    refactored to use O(n) time

--- best_time_to_buy_sell_stock.rb
+++ best_time_to_buy_sell_stock.rb
@@ -11,14 +11,27 @@ def max_profit(prices)
       sell_day = day + 1
       sell_day.upto(prices.size - 1) do |selling_day|
           selling_price = prices[selling_day]
-          if selling_price > buying_price
-              profit = selling_price - buying_price
-              max_profit = profit if profit > max_profit 
-          end
+          next if selling_price < buying_price
+            profit = selling_price - buying_price
+            max_profit = profit if profit > max_profit 
       end
   end
   max_profit
 end
 
-p max_profit([7,6,4,3,1])
-p max_profit([7,1,5,3,6,4])
+def max_profit_2(prices)
+  max_profit = 0
+  stop = prices.size - 2
+  0.upto(stop) do |indx|
+    buy_at = prices[indx]
+    sell = prices[indx + 1...prices.size].max
+    p "buy at #{buy_at} sell #{sell}"
+    next if sell < buy_at
+    profit = sell - buy_at
+    max_profit = profit if profit > max_profit 
+  end
+  max_profit
+end
+
+p max_profit_2([7,6,4,3,1])
+p max_profit_2([7,1,5,3,6,4])
