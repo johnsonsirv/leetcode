6f22cb92d76610c0ff271ccf7de33015161cfedd
Victor Johnson <johnsonsirv@gmail.com>
2019-07-31 11:04:14 +0200

    added code logic to find maximum profit

---
+++ best_time_to_buy_sell_stock.rb
@@ -0,0 +1,23 @@
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit(prices)
+  max_profit = 0
+  # constraints: selling price needs to be larger than buying price
+  # for each day, pick price, try to sell it on the other days after the current day
+  # once the selling price of any day is less than the current buying price, skip to next day
+  # else, if the profit is larger than the current max_profit, then update the the max profit
+  # return max_profit
+  prices.each_with_index do |buying_price, day|
+      sell_day = day + 1
+      sell_day.upto(prices.size - 1) do |selling_day|
+          selling_price = prices[selling_day]
+          if selling_price > buying_price
+              profit = selling_price - buying_price
+              max_profit = profit if profit > max_profit 
+          end
+      end
+  end
+  max_profit
+end
+
+max_profit([7,6,4,3,1])
