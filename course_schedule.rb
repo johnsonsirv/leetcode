97429b2d8e698666c97eae793648946cb5acd385
Victor Johnson <johnsonsirv@gmail.com>
2019-08-21 15:13:07 +0200

    course_schedule initial commit

---
+++ course_schedule.rb
@@ -0,0 +1,7 @@
+def can_finish(num_courses, prerequisites)
+  #search the graph of pre-requisites in a certain order
+  # keep track of the number of times you visited each EDGE (course)
+  # if it is <= num_of_courses then return true; else return false
+ p prerequisites
+  
+end
