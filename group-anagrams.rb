56c27e6ddcee118aa0fe963089e2e286b2dce9da
Victor Johnson <johnsonsirv@gmail.com>
2019-09-09 20:33:06 +0200

    added 2 definitions

--- group-anagrams.rb
+++ group-anagrams.rb
@@ -13,9 +13,28 @@
     grouped_ana.values
 end
 
+def group_anagrams_2(strs)
+    grouped_ana = {}
+    strs.each do |w|
+      s = w.chars.sort.join('')
+      # p s
+     if grouped_ana.has_key?(s)
+         grouped_ana[s] << w
+     else
+         grouped_ana[s] = [w]
+     end 
+    end
+    grouped_ana.values
+end
 
-p group_anagrams(["","b"])
-p group_anagrams(["",""])
-p group_anagrams(["and","dan"])
-p group_anagrams(["c","c"])
-p group_anagrams(["","b",""])
+
+def group_anagrams_3(strs)
+ strs.group_by {|w| w.chars.sort}.values
+end
+
+
+# p group_anagrams_2(["","b"])
+# p group_anagrams(["",""])
+p group_anagrams_2(["and","dan"])
+# p group_anagrams(["c","c"])
+# p group_anagrams(["","b",""])
